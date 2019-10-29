3edd60435ae575ae508218437e8d12df2c38e22a
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-10-29 19:10:54 -0300

    tela de doação

--- src/components/MenuOng.vue
+++ src/components/MenuOng.vue
@@ -1,47 +1,47 @@
 <template>
-<div>
-        <header class="header-one">
-            <!-- header-area start -->
-            <div id="sticker" class="header-area hidden-xs">
-                <div class="container">
-                    <div class="row">
-                        <!-- logo start -->
-                        <div class="col-md-3 col-sm-3">
-                            <div class="logo">
-                                <!-- Brand -->
-                                <a class="navbar-brand page-scroll black-logo" @click="goToHome">
-                                    <img src="img/logo/logo2.png">
-                                </a>
-                                <a class="navbar-brand page-scroll white-logo" href="#">
-                                    <img src="img/logo/logo2.png" alt="">
-                                </a>
-                            </div>
-                            <!-- logo end -->
+<div class="menu-ong">
+    <header class="header-one">
+        <!-- header-area start -->
+        <div id="sticker" class="header-area hidden-xs">
+            <div class="container">
+                <div class="row">
+                    <!-- logo start -->
+                    <div class="col-md-3 col-sm-3">
+                        <div class="logo">
+                            <!-- Brand -->
+                            <a class="navbar-brand page-scroll black-logo" @click="goToHome">
+                                <img src="img/logo/logo2.png">
+                            </a>
+                            <a class="navbar-brand page-scroll white-logo" href="#">
+                                <img src="img/logo/logo2.png" alt="">
+                            </a>
                         </div>
-                        <div class="col-md-9 col-sm-9">
-                            <div class="header-right-link">
-                                <!-- search option end -->
-                        <a  style="color:white" class="s-menu" href="#">Login</a>
-                            </div>
-                            <!-- mainmenu start -->
-                            <nav class="navbar navbar-default">
-                                <div class="collapse navbar-collapse" id="navbar-example">
-                                    <div class="main-menu">
-                                        <ul class="nav navbar-nav navbar-right">
-                                            <li><a class="pagess" @click="goToHome()">Início</a></li>
-                                            <li><a href="#">Sobre</a></li>
-                                            <li><a class="pagess" href="#">Galeria</a></li>
-                                        </ul>
-                                    </div>
+                        <!-- logo end -->
+                    </div>
+                    <div class="col-md-9 col-sm-9">
+                        <div class="header-right-link">
+                            <!-- search option end -->
+                    <a  style="color:white" class="s-menu" href="#">Login</a>
+                        </div>
+                        <!-- mainmenu start -->
+                        <nav class="navbar navbar-default">
+                            <div class="collapse navbar-collapse" id="navbar-example">
+                                <div class="main-menu">
+                                    <ul class="nav navbar-nav navbar-right">
+                                        <li><a class="pagess" @click="goToHome()">Início</a></li>
+                                        <li><a href="#">Sobre</a></li>
+                                        <li><a class="pagess" href="#">Galeria</a></li>
+                                    </ul>
                                 </div>
-                            </nav>
-                            <!-- mainmenu end -->
-                        </div>
+                            </div>
+                        </nav>
+                        <!-- mainmenu end -->
                     </div>
                 </div>
             </div>
-            <!-- header-area end -->
-        </header>		
+        </div>
+        <!-- header-area end -->
+    </header>		
     <!-- header end -->
 </div>
 </template>
