8996a86dfab63f74c8c232274c73343d274363f5
Lorran <ptklorran@gmail.com>
2019-11-03 16:53:02 -0200

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,51 +1,51 @@
 <template>
-  <div class="centraliza intro-home">
-    <div class="bg-wrapper">
-        <img src="img/background/bg1.png" alt="">
-    </div>
-			<div style="justify-content: center; align-items: center" class="intro-content">
-				<div class="slider-content">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-6 col-sm-6 col-xs-12">
+	<div class="centraliza intro-home">
+		<div class="bg-wrapper">
+			<img src="img/background/bg1.png" alt="">
+		</div>
+		<div style="justify-content: center; align-items: center" class="intro-content">
+			<div class="slider-content">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							
+							<div class="ajustar-meio hidden-sm-and-up layer-1 wow fadeInUp" data-wow-delay="0.3s">
 								
-								<div class="ajustar-meio hidden-sm-and-up layer-1 wow fadeInUp" data-wow-delay="0.3s">
-									
-									<v-spacer></v-spacer>
-									<img width="70" src="img/slider/s1a.png" alt="">	
-									<v-spacer></v-spacer>
+								<v-spacer></v-spacer>
+								<img width="70" src="img/slider/s1a.png" alt="">	
+								<v-spacer></v-spacer>
 
-								</div>
-								<!-- layer 1 -->
-								<div class="layer-1 wow fadeInUp" data-wow-delay="0.3s">
-									<h2 class="title2">Olá, eu sou o Leon!</h2>
-								</div>
-								<!-- layer 2 -->
-								<div class="layer-2 wow fadeInUp" data-wow-delay="0.5s">
-									<p>O Advogato adotado pela OAB/AP, como diretor executivo do instituto Dr. Leon, convido você a conhecer a nossa causa e se tornar oficialmente um amigo dos animais.</p>
-								</div>
-								<!-- layer 3 -->
-								<div class="layer-3 wow fadeInUp" data-wow-delay="0.7s">
-									<a @click="scrollar(780)" style="color:white;" class="ready-btn left-btn" >Saiba mais sobre a nossa missão</a>
-								</div>
 							</div>
-							<div class="col-md-6 col-sm-6 hidden-xs">
-								<div class="slide-images-inner wow fadeInUp" data-wow-delay="0.5s">
-                  <div class="slide-top-image item-bounce">
-                  </div>
-                  <div class="slide-images">
-											<div class="hidden-sm-and-up" id="leon-mobile">
-                      	<img src="img/slider/s1a.png" alt="">	
-											</div>
-                      <img src="img/slider/s1a.png" alt="">
-                  </div>
-								</div>
+							<!-- layer 1 -->
+							<div class="layer-1 wow fadeInUp" data-wow-delay="0.3s">
+								<h2 class="title2">Olá, eu sou o Leon!</h2>
+							</div>
+							<!-- layer 2 -->
+							<div class="layer-2 wow fadeInUp" data-wow-delay="0.5s">
+								<p>O Advogato adotado pela OAB/AP, como diretor executivo do instituto Dr. Leon, convido você a conhecer a nossa causa e se tornar oficialmente um amigo dos animais.</p>
+							</div>
+							<!-- layer 3 -->
+							<div class="layer-3 wow fadeInUp" data-wow-delay="0.7s">
+								<a @click="scrollar(780)" style="color:white;" class="ready-btn left-btn" >Saiba mais sobre a nossa missão</a>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6 hidden-xs">
+							<div class="slide-images-inner wow fadeInUp" data-wow-delay="0.5s">
+				<div class="slide-top-image item-bounce">
+				</div>
+				<div class="slide-images">
+										<div class="hidden-sm-and-up" id="leon-mobile">
+					<img src="img/slider/s1a.png" alt="">	
+										</div>
+					<img src="img/slider/s1a.png" alt="">
+				</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-  </div>
+		</div>
+	</div>
 </template>
 
 <script>
--- src/components/Missao.vue
+++ src/components/Missao.vue
@@ -1,5 +1,4 @@
 <template>
-<div class="about-area  fix area-padding">
   <v-container>
     <v-row>
       <v-col cols="12" md="6" sm="6" xs="12">
@@ -8,44 +7,44 @@
             <div class="rotmate-image rotateme">
                 <img src="img/about/circle.png" alt="">
             </div>
-            <img src="img/about/ab.png" alt="">
-            <div class="video-content">
-                <a href="https://www.youtube.com/watch?v=O33uuBh6nXA" class="video-play vid-zone">
-                </a>
-            </div>
-            </div>
-          </div>
-        </v-col>
 
-        <v-col cols="12" md="6" sm="6" xs="12">
-           <div class="support-all">
-            <!-- Start services -->
-            <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
-                <a class="support-images" href="#">01</a>
-                <div class="support-content">
-                    <h4><a href="#">Professional Services</a></h4>
-                    <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
-                </div>
-            </div>
-            <!-- Start services -->
-            <div class="support-services wow fadeInUp" data-wow-delay="0.5s">
-                <a class="support-images" href="#">02</a>
-                <div class="support-content">
-                    <h4><a href="#">Safe & Security</a></h4>
-                    <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then</p>
-                </div>
-            </div>
-            <!-- Start services -->
-            <div class="support-services hidden-sm wow fadeInUp" data-wow-delay="0.7s">
-                <a class="support-images" href="#">03</a>
-                <div class="support-content">
-                    <h4><a href="#">Live Support</a></h4>
-                    <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
-                </div>
+            <img src="img/about/ab.png" alt="">
+
+            <div class="video-content">
+                <a href="https://www.youtube.com/watch?v=O33uuBh6nXA" class="video-play vid-zone"></a>
             </div>
           </div>
-        </v-col>     
-      </v-row>
-    </v-container>
-</div>
+        </div>
+      </v-col>
+
+      <v-col cols="12" md="6" sm="6" xs="12">
+        <div class="support-all">
+          <!-- Start services -->
+          <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
+              <a class="support-images" href="#">01</a>
+              <div class="support-content">
+                  <h4><a href="#">Professional Services</a></h4>
+                  <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
+              </div>
+          </div>
+          <!-- Start services -->
+          <div class="support-services wow fadeInUp" data-wow-delay="0.5s">
+            <a class="support-images" href="#">02</a>
+            <div class="support-content">
+                <h4><a href="#">Safe & Security</a></h4>
+                <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then</p>
+            </div>
+          </div>
+          <!-- Start services -->
+          <div class="support-services hidden-sm wow fadeInUp" data-wow-delay="0.7s">
+            <a class="support-images" href="#">03</a>
+            <div class="support-content">
+              <h4><a href="#">Live Support</a></h4>
+              <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
+            </div>
+          </div>
+        </div>
+      </v-col>     
+    </v-row>
+  </v-container>
 </template>
