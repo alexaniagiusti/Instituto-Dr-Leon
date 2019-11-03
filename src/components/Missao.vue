990ab9c7f324398fcbd7d0741a82101800fedc35
Lorran <ptklorran@gmail.com>
2019-11-03 18:02:31 -0200

    fixx

--- src/components/Missao.vue
+++ src/components/Missao.vue
@@ -1,50 +1,50 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" md="6" sm="6" xs="12">
-        <div class="about-content">
-          <div class="about-images wow fadeInLeft" data-wow-delay="0.7s">
-            <div class="rotmate-image rotateme">
-                <img src="img/about/circle.png" alt="">
-            </div>
-
-            <img src="img/about/ab.png" alt="">
-
-            <div class="video-content">
-                <a href="https://www.youtube.com/watch?v=O33uuBh6nXA" class="video-play vid-zone"></a>
-            </div>
-          </div>
-        </div>
-      </v-col>
-
-      <v-col cols="12" md="6" sm="6" xs="12">
-        <div class="support-all">
-          <!-- Start services -->
-          <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
-              <a class="support-images" href="#">01</a>
-              <div class="support-content">
-                  <h4><a href="#">Professional Services</a></h4>
-                  <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
+    <div class="fix area-padding">
+      <v-row>
+        <v-col cols="12" md="6" sm="6" xs="12">
+          <div class="about-content">
+            <div class="about-images fadeInLeft" data-wow-delay="0.7s">
+              <div class="rotmate-image rotateme">
+                  <img src="img/about/circle.png" alt="">
               </div>
-          </div>
-          <!-- Start services -->
-          <div class="support-services wow fadeInUp" data-wow-delay="0.5s">
-            <a class="support-images" href="#">02</a>
-            <div class="support-content">
-                <h4><a href="#">Safe & Security</a></h4>
-                <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then</p>
+
+              <img src="img/about/ab.png" alt="">
+
+              <div class="video-content">
+                  <a href="https://www.youtube.com/watch?v=O33uuBh6nXA" class="video-play vid-zone"></a>
+              </div>
             </div>
           </div>
-          <!-- Start services -->
-          <div class="support-services hidden-sm wow fadeInUp" data-wow-delay="0.7s">
-            <a class="support-images" href="#">03</a>
-            <div class="support-content">
-              <h4><a href="#">Live Support</a></h4>
-              <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
+        </v-col>
+
+        <v-col cols="12" md="6" sm="6" xs="12">
+          <div class="support-all">
+            <!-- Start services -->
+            <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
+                <a class="support-images" href="#">01</a>
+                <div class="support-content">
+                    <h4><a href="#">Professional Services</a></h4>
+                    <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
+                </div>
+            </div>
+            <!-- Start services -->
+            <div class="support-services wow fadeInUp" data-wow-delay="0.5s">
+              <a class="support-images" href="#">02</a>
+              <div class="support-content">
+                  <h4><a href="#">Safe & Security</a></h4>
+                  <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then</p>
+              </div>
+            </div>
+            <!-- Start services -->
+            <div class="support-services hidden-sm wow fadeInUp" data-wow-delay="0.7s">
+              <a class="support-images" href="#">03</a>
+              <div class="support-content">
+                <h4><a href="#">Live Support</a></h4>
+                <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
+              </div>
             </div>
           </div>
-        </div>
-      </v-col>     
-    </v-row>
-  </v-container>
+        </v-col>     
+      </v-row>
+    </div>
 </template>
