1ae4fdcbe83c4b5fa0e6b8b6b5e4b17686a9b2c0
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-11-04 20:07:26 -0300

    apresentação da missão do instituto

--- src/components/Missao.vue
+++ src/components/Missao.vue
@@ -1,50 +1,48 @@
 <template>
+  <div id="background-missao">
     <div class="fix area-padding">
-      <v-row>
-        <v-col cols="12" md="6" sm="6" xs="12">
-          <div class="about-content">
-            <div class="about-images fadeInLeft" data-wow-delay="0.7s">
-              <div class="rotmate-image rotateme">
-                  <img src="img/about/circle.png" alt="">
-              </div>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6" sm="6" xs="12">
+          <img src="img/about/ab.png" alt="">
 
-              <img src="img/about/ab.png" alt="">
+          </v-col>
 
-              <div class="video-content">
-                  <a href="https://www.youtube.com/watch?v=O33uuBh6nXA" class="video-play vid-zone"></a>
-              </div>
-            </div>
-          </div>
-        </v-col>
-
-        <v-col cols="12" md="6" sm="6" xs="12">
-          <div class="support-all">
+          <v-col cols="12" md="6" sm="6" xs="12">
+            <div class="support-all">
             <!-- Start services -->
-            <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
-                <a class="support-images" href="#">01</a>
-                <div class="support-content">
-                    <h4><a href="#">Professional Services</a></h4>
-                    <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
+              <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
+                <div class="support-content pa-4 ma-0">
+                  <h4><a href="#">1ª Aventura do Dr Leon.</a></h4>
+                  <p style="text-align: justify;">A missão do Instituto Dr Leon, é ajudar Ogns que protegem animais maltratados por todo o país, começando pela nossa cidade Macapá, mas para isso precisamos da sua ajuda, nosso objetivo inicial é ajudar as nossas <a href="#">Ongs parceiras</a> a quitarem as suas dívidas em clínicas veterinárias, dívidas essas que foram contraídas devido ao trabalho incansável e resgate de animais de rua. 
+                  As Ongs beneficiadas com a 1ª aventura do Dr. Leon são:
+                  </p>
+                  <p class="mt-4">
+                  <v-icon>mdi-paw</v-icon> Gateiros Tucujus: Dívida de 3.500.
+                  </p>
+                  <p class="mt-4">
+                  <v-icon>mdi-paw</v-icon> Ong Salvação: Dívida de 8 mil reais.
+                  </p>
+                  <p class="mt-4">
+                  <v-icon>mdi-paw</v-icon> Ong Vira-Latas: Dívida de 5 mil reais.
+                  </p>
+                  <p class="mt-4">
+                  <v-icon>mdi-paw</v-icon> Ong Anjos Protetores: Dívida de 8 mil reais.
+                  </p>
+                  <v-btn class="mt-4" color="blue" block><span class="white--text">Contribua</span><v-icon color="white" class="ma-2">mdi-hand-heart</v-icon> </v-btn>
                 </div>
-            </div>
-            <!-- Start services -->
-            <div class="support-services wow fadeInUp" data-wow-delay="0.5s">
-              <a class="support-images" href="#">02</a>
-              <div class="support-content">
-                  <h4><a href="#">Safe & Security</a></h4>
-                  <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then</p>
               </div>
             </div>
-            <!-- Start services -->
-            <div class="support-services hidden-sm wow fadeInUp" data-wow-delay="0.7s">
-              <a class="support-images" href="#">03</a>
-              <div class="support-content">
-                <h4><a href="#">Live Support</a></h4>
-                <p>Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define. define their new business objectives and then.</p>
-              </div>
-            </div>
-          </div>
-        </v-col>     
-      </v-row>
+          </v-col>     
+        </v-row>
+      </v-container>
     </div>
+  </div>
 </template>
+
+
+<style scoped>
+#background-missao {
+background-color: #fff;
+}
+</style>
