00dfa9f10eb25ad551e826fa018c8f43ec7d785e
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-11-06 14:19:14 -0300

    tela que exibe a missão do instituto

--- src/components/Missao.vue
+++ src/components/Missao.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="background-missao">
-    <div class="fix area-padding">
-      <v-container>
+      <p style="padding: 10px;height: 50px; background-color: #eae9fb" class="headline black--text text-center">Missão do Instituto Dr. Leon</p>
+    <div>
+      <v-container class="mb-0 pa-0">
         <v-row>
           <v-col cols="12" md="6" sm="6" xs="12">
-          <img src="img/about/ab.png" alt="">
+          <img  src="img/about/ab.png" alt="">
 
           </v-col>
 
@@ -14,22 +15,22 @@
               <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
                 <div class="support-content pa-4 ma-0">
                   <h4><a href="#">1ª Aventura do Dr Leon.</a></h4>
-                  <p style="text-align: justify;">A missão do Instituto Dr Leon, é ajudar Ogns que protegem animais maltratados por todo o país, começando pela nossa cidade Macapá, mas para isso precisamos da sua ajuda, nosso objetivo inicial é ajudar as nossas <a href="#">Ongs parceiras</a> a quitarem as suas dívidas em clínicas veterinárias, dívidas essas que foram contraídas devido ao trabalho incansável e resgate de animais de rua. 
+                  <p style="text-align: justify;" class="black--text">Nossa missão é ajudar Ogns que protegem animais maltratados por todo o país, começando pela nossa cidade Macapá, mas para isso precisamos da sua ajuda, nosso objetivo inicial é ajudar as nossas <a href="#">Ongs Parceiras</a> a quitarem as suas dívidas em clínicas veterinárias, dívidas essas que foram contraídas devido ao trabalho incansável e resgate de animais de rua. 
                   As Ongs beneficiadas com a 1ª aventura do Dr. Leon são:
                   </p>
-                  <p class="mt-4">
+                  <p class="mt-4 black--text">
                   <v-icon>mdi-paw</v-icon> Gateiros Tucujus: Dívida de 3.500.
                   </p>
-                  <p class="mt-4">
+                  <p class="mt-4 black--text">
                   <v-icon>mdi-paw</v-icon> Ong Salvação: Dívida de 8 mil reais.
                   </p>
-                  <p class="mt-4">
+                  <p class="mt-4 black--text">
                   <v-icon>mdi-paw</v-icon> Ong Vira-Latas: Dívida de 5 mil reais.
                   </p>
-                  <p class="mt-4">
+                  <p class="mt-4 black--text">
                   <v-icon>mdi-paw</v-icon> Ong Anjos Protetores: Dívida de 8 mil reais.
                   </p>
-                  <v-btn class="mt-4" color="blue" block><span class="white--text">Contribua</span><v-icon color="white" class="ma-2">mdi-hand-heart</v-icon> </v-btn>
+                  <v-btn class="mt-4" color="blue" block><span class="white--text">Contribua</span><v-icon color="white" class="ma-2">mdi-charity</v-icon> </v-btn>
                 </div>
               </div>
             </div>
@@ -44,5 +45,6 @@
 <style scoped>
 #background-missao {
 background-color: #fff;
+
 }
 </style>
