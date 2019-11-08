4f7415fddc893f89ae23812410b14f32c5e2699e
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-11-08 10:51:46 -0300

    ajuste na exibição da tela missão

--- src/components/Missao.vue
+++ src/components/Missao.vue
@@ -1,19 +1,54 @@
 <template>
-  <div id="background-missao">
-      <p style="padding: 10px;height: 50px; background-color: #eae9fb" class="headline black--text text-center">Missão do Instituto Dr. Leon</p>
+<div>
+<div class="missao-mobile hidden-sm-and-up" style="background-color: #fff">
+  <v-container class="ma-0 pa-0">
+    <v-row>
+      <v-col cols="12">
+        <p style="padding: 10px; background-color: #fff" class="font-weight-light headline black--text">Missão do Instituto Dr. Leon!</p>
+        <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
+                <div class="support-content pa-4 ma-0">
+                  <img class="mb-3" src="img/about/ab.png" alt="">
+                  <h4><a href="#" class="mt-4">1ª Aventura do Dr Leon.</a></h4>
+                  <p style="text-align: justify;" class="black--text">Nossa missão é ajudar Ogns que protegem animais maltratados por todo o país, começando pela nossa cidade Macapá, mas para isso precisamos da sua ajuda, nosso objetivo inicial é ajudar as nossas <a href="#">Ongs Parceiras</a> a quitarem as suas dívidas em clínicas veterinárias, dívidas essas que foram contraídas devido ao trabalho incansável e resgate de animais de rua. 
+                  As Ongs beneficiadas com a 1ª aventura do Dr. Leon são:
+                  </p>
+                  <p class="mt-4 black--text">
+                  <v-icon>mdi-paw</v-icon> Gateiros Tucujus: Dívida de 3.500.
+                  </p>
+                  <p class="mt-4 black--text">
+                  <v-icon>mdi-paw</v-icon> Ong Salvação: Dívida de 8 mil reais.
+                  </p>
+                  <p class="mt-4 black--text">
+                  <v-icon>mdi-paw</v-icon> Ong Vira-Latas: Dívida de 5 mil reais.
+                  </p>
+                  <p class="mt-4 black--text">
+                  <v-icon>mdi-paw</v-icon> Ong Anjos Protetores: Dívida de 8 mil reais.
+                  </p>
+                  <v-btn class="mt-4" color="blue" block><span class="white--text">Contribua</span><v-icon color="white" class="ma-2">mdi-charity</v-icon> </v-btn>
+                </div>
+              </div>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <!-- Container de exibição para desktop -->
+</div>
+
+  <div id="background-missao" class="hidden-sm-and-down">
+      <p style="padding: 10px;height: 50px; background-color: #eae9fb" class="hidden-sm-and-down font-weight-light headline black--text text-center">Missão do Instituto Dr. Leon</p>
     <div>
-      <v-container class="mb-0 pa-0">
+      <v-container>
         <v-row>
-          <v-col cols="12" md="6" sm="6" xs="12">
+          <v-col cols="12" md="6" sm="6" xs="12" class="hidden-sm-and-down">
+          <p style="height: 50px; background-color: #fff" class="hidden-sm-and-up font-weight-light headline black--text text-center">Missão do Instituto Dr. Leon</p>
           <img  src="img/about/ab.png" alt="">
-
           </v-col>
-
           <v-col cols="12" md="6" sm="6" xs="12">
             <div class="support-all">
             <!-- Start services -->
               <div class="support-services wow fadeInUp" data-wow-delay="0.3s">
                 <div class="support-content pa-4 ma-0">
+                  <img class="mb-3 hidden-sm-and-up" src="img/about/ab.png" alt="">
                   <h4><a href="#">1ª Aventura do Dr Leon.</a></h4>
                   <p style="text-align: justify;" class="black--text">Nossa missão é ajudar Ogns que protegem animais maltratados por todo o país, começando pela nossa cidade Macapá, mas para isso precisamos da sua ajuda, nosso objetivo inicial é ajudar as nossas <a href="#">Ongs Parceiras</a> a quitarem as suas dívidas em clínicas veterinárias, dívidas essas que foram contraídas devido ao trabalho incansável e resgate de animais de rua. 
                   As Ongs beneficiadas com a 1ª aventura do Dr. Leon são:
@@ -39,6 +74,7 @@
       </v-container>
     </div>
   </div>
+</div>
 </template>
 
 
